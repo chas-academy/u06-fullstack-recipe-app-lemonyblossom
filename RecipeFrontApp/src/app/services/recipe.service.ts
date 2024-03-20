e2ee56ec76d2e41a3754f8a2c3f77ab95c9d2a79
Emma Jensen <emmasarahlinnea@gmail.com>
2024-03-20 14:30:35 +0000

    recipe.service error

--- RecipeFrontApp/src/app/services/recipe.service.ts
+++ RecipeFrontApp/src/app/services/recipe.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +15,16 @@ export class RecipeService {
   private httpOptions = {
     headers: new HttpHeaders({
       'accept': 'application/json',
-      'Accept language': 'en'
+      'Accept-Language': 'en'
     })
   }
+
   constructor(private http: HttpClient) { }
 
-  getRecipes(searcgterm: string): Observable<any> {
-    let quisinetype =
-      let mealType =
-        let url = this.baseUrl + "&q=" + searchterm + "&app_id" + this.app_id + "&app_key=" + 
+  getRecipes(searchterm: string): Observable<any> {
+    let quisineType = "American";
+    let mealType = "Dinner";
+    let url = this.baseUrl + "&q=" + searchterm + "&app_id" + this.app_id + "&app_key=" + this.app_key + "&cuisineType=" + quisineType + "&mealType" + mealType;
+    return this.http.get<any>(url, this.httpOptions);
   }
 }
