03f87074e0619f9473b6b6c1be49e2c37bc8f608
Emma Jensen <emmasarahlinnea@gmail.com>
2024-03-25 15:17:41 +0100

    html for search compnent- trying to figure out why .value does not work in the input, also included my httpclient in config, I removed it by accident earlier

--- RecipeFrontApp/src/app/app.routes.ts
+++ RecipeFrontApp/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { RecipeResponse } from './interfaces/recipe';
+import { RecipeSearchComponent } from './pages/search/recipe-search.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+   { path: 'search', component: RecipeSearchComponent },
+   { path: 'recipe', component: RecipeComponent },
+   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
 ];
--- RecipeFrontApp/src/app/pages/search/recipe-search.component.ts
+++ RecipeFrontApp/src/app/pages/search/recipe-search.component.ts
@@ -1,43 +1,23 @@
 import { Component } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
+import { RecipeResponse } from '../../interfaces/recipe';
 
 @Component({
   selector: 'app-recipe-search',
-  standalone: true,
-  imports: [],
   templateUrl: './recipe-search.component.html',
-  styleUrl: './recipe-search.component.css'
+  styleUrls: ['./recipe-search.component.css']
 })
 
 export class RecipeSearchComponent {
-
-  recipes? = any;
+  searchTerm: string = '';
+  recipes: RecipeResponse[] = [];
 
   constructor(private recipeService: RecipeService) { }
 
   searchRecipe() {
-    this.recipeService.getRecipes("chicken").subscribe(res => {
+    this.recipeService.getRecipes(this.searchTerm).subscribe(res => {
       console.log(res);
       this.recipes = res;
-
-      let recipeArray = any[];
-      recipeArray = res.hits;
-      console.log(recipeArray);
-
-      let recipes = recipeArray.map(item => {
-        return {
-          self: item._links.self.href,
-          label: item.recipe.label,
-          image: item.recipe.image,
-          totalTime: item.recipe.totalTime,
-          ingredientLines: item.recipe.ingredientLines
-          //lägg in fler om du vill
-        }
-      })
-      console.log(recipes);
-      this.recipes.recipes;
     });
-
   }
-
 }
